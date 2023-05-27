754221e3db351b30e4807b2181ec62453e5e32d3
Junaid <jia1976@gmail.com>
2023-05-27 10:47:06 +0200

    updated deploy.yaml for dev again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yaat wala
+          booby custard where is my pa
         </p>
         <a
           className="App-link"
